8de0d0acc63fd6b4ca9bbf8b39d0cf6583b38a77
AABur <aabur@mail.ru>
2021-04-11 19:41:48 +0300

    feat: :sparkles: update simple page downloader

--- page_loader/download.py
+++ page_loader/download.py
@@ -1,4 +1,28 @@
-# -*- coding:utf-8 -*-
+import os
+import re
+import sys
+from pathlib import Path, PurePath
+from urllib.parse import urlsplit
 
-def download(url, output_dir='.'):
-    return '/var/tmp/ru-hexlet-io-courses.html'
+import requests
+
+
+def download(page_url, output_dir):
+    if not os.access(output_dir, os.W_OK):
+        sys.exit('ERROR !!!')
+    response = requests.get(page_url)
+    file_name = create_file_name(page_url)
+    file_path = Path(PurePath(output_dir, file_name))
+    file_path.write_text(response.text)
+    with open(file_path, 'wb') as fd:
+        for chunk in response.iter_content(chunk_size=128):  # noqa: WPS432
+            fd.write(chunk)
+    return file_path
+
+
+def create_file_name(page_url):
+    split = urlsplit(page_url)
+    nl = re.sub(r'[\W]', '-', split.netloc)  # noqa: W605
+    pa = re.sub(r'[\W]', '-', split.path)  # noqa: W605
+    jj = [nl, pa, '.html']
+    return ''.join(jj)
