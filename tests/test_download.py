a04ca07512147d8abe32b672a5f24facfff88efc
AABur <aabur@mail.ru>
2021-04-11 20:02:44 +0300

    test: :white_check_mark: add downloader test

--- tests/test_download.py
+++ tests/test_download.py
@@ -1,8 +1,13 @@
 # -*- coding:utf-8 -*-
 
-from page_loader import download
+from page_loader.download import create_file_name, download
 
 
-def test_file_path():
+def test_create_file_name():
+    file_name = create_file_name('https://ru.hexlet.io/courses')
+    assert file_name == 'ru-hexlet-io-courses.html'
+
+
+def test_create_name():
     file_path = download('https://ru.hexlet.io/courses', '/var/tmp')
-    assert file_path == '/var/tmp/ru-hexlet-io-courses.html'
+    assert str(file_path) == '/var/tmp/ru-hexlet-io-courses.html'
