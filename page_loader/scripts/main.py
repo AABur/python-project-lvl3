b32a5bada0e8be31d40fceb2ee675dfdcae961de
AABur <aabur@mail.ru>
2021-04-11 19:38:45 +0300

    refactor: :recycle: refactor main

--- page_loader/scripts/main.py
+++ page_loader/scripts/main.py
@@ -6,8 +6,7 @@ from page_loader.arg_parser import arg_parser
 
 def main():
     args = arg_parser().parse_args()
-    print(
-        download(args.page_url, args.output_dir))
+    print(download(args.page_url, args.output_dir))
 
 
 if __name__ == '__main__':
