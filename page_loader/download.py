45c83010917f995fe5fc6544b3fea1b96d8841aa
AABur <aabur@mail.ru>
2021-04-12 22:28:05 +0300

    refactor: :fire: remove unused code

--- page_loader/download.py
+++ page_loader/download.py
@@ -1,6 +1,4 @@
-import os
 import re
-import sys
 from pathlib import Path, PurePath
 from urllib.parse import urlsplit
 
@@ -12,15 +10,12 @@ def download(page_url, output_dir):
     file_name = create_file_name(page_url)
     file_path = Path(PurePath(output_dir, file_name))
     file_path.write_text(response.text)
-    with open(file_path, 'wb') as fd:
-        for chunk in response.iter_content(chunk_size=128):  # noqa: WPS432
-            fd.write(chunk)
     return file_path
 
 
 def create_file_name(page_url):
     split = urlsplit(page_url)
-    nl = re.sub(r'[\W]', '-', split.netloc)  # noqa: W605
-    pa = re.sub(r'[\W]', '-', split.path)  # noqa: W605
+    nl = re.sub(r'[\W]', '-', split.netloc)
+    pa = re.sub(r'[\W]', '-', split.path)
     jj = [nl, pa, '.html']
     return ''.join(jj)
