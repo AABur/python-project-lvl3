4a4583fe0255073fe4da98d79f0379c7ad4c7493
AABur <aabur@mail.ru>
2021-04-13 23:20:39 +0300

    feat: :sparkles: add images scrapping

--- page_loader/download.py
+++ page_loader/download.py
@@ -3,12 +3,17 @@ from pathlib import Path, PurePath
 from urllib.parse import urlsplit
 
 import requests
+from bs4 import BeautifulSoup
 
 
 def download(page_url, output_dir):
-    response = requests.get(page_url)
     file_name = create_file_name(page_url)
     file_path = Path(PurePath(output_dir, file_name))
+    response = requests.get(page_url)
+    soup = BeautifulSoup(response.content, 'lxml')
+    images = soup.find_all('img')
+    for image in images:
+        print(image.get('src'))
     file_path.write_text(response.text)
     return file_path
 
