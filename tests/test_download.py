cc77e9232086cdfa4c516d0922ef2cd0ffe1769b
AABur <aabur@mail.ru>
2021-04-28 12:58:38 +0300

    test: :white_check_mark: update tests

--- tests/test_download.py
+++ tests/test_download.py
@@ -6,17 +6,43 @@
 # 'requests_mock' fixture
 # https://requests-mock.readthedocs.io/en/latest/pytest.html
 
+import pytest
+from pathlib import PurePath
+
 from page_loader.download import create_file_name, download
 
 
-def test_create_file_name():
-    file_name = create_file_name('https://ru.hexlet.io/courses')
-    assert file_name == 'ru-hexlet-io-courses.html'
+@pytest.mark.parametrize(
+    'file_url, file_name',
+    [
+        ('https://ru.hexlet.io/courses', 'ru-hexlet-io-courses.html'),
+        ('/assets/application.css', 'ru-hexlet-io-assets-application.css'),
+        (
+            '/assets/professions/prof_python.png',
+            'ru-hexlet-io-assets-professions-prof-python.png',
+        ),
+        (
+            'https://ru.hexlet.io/packs/js/runtime.js',
+            'ru-hexlet-io-packs-js-runtime.js',
+        ),
+    ],
+)
+def test_create_file_name(file_url, file_name):
+    created_file_name = create_file_name(file_url)
+    assert file_name == created_file_name
 
 
-def test_download(tmpdir, requests_mock):
-    requests_mock.get('http://test.com', text='data')
-    file_path = download('http://test.com', tmpdir)
+def test_download_img(tmpdir, requests_mock):
+    requested_img_file = PurePath('tests/fixtures/received/prof_python.png')
+    expected_img_file = PurePath(
+        'tests/fixtures/expected/ru-hexlet-io-courses_files/ru-hexlet-io-assets-professions-prof-python.png',  # noqa E501
+    )
+    requests_mock.get(
+        '/assets/professions/prof_python.png',
+        content=requested_img_file,
+    )
+    file_path = download(
+        'https://ru.hexlet.io/assets/professions/prof_python.png', tmpdir,
+    )
     file_content = file_path.read_text()
-    assert file_path.is_file() is True
-    assert file_content == 'data'
+    assert expected_img_file == requested_img_file
