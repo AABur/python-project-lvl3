df716f055dfac76f79fb0025e075f03b66a0ff0f
AABur <aabur@mail.ru>
2021-05-05 02:11:24 +0200

    feat: :sparkles: add create dir function

--- page_loader/download.py
+++ page_loader/download.py
@@ -3,21 +3,31 @@ from pathlib import Path, PurePath
 from urllib.parse import urlsplit
 
 import requests
-from bs4 import BeautifulSoup
 
 
-def download(page_url, output_dir):
-    # download html
+def download(page_url: str, output_dir: str) -> Path:
+    page_url.rstrip('.')
+    print(page_url)
     file_name = create_file_name(page_url)
     file_path = Path(PurePath(output_dir, file_name))
     response = requests.get(page_url)
     file_path.write_bytes(response.content)
+    dir_name = create_dir_name(page_url)
+    Path(output_dir, dir_name).mkdir(exist_ok=True)
     return file_path
 
 
-def create_file_name(page_url):
+def create_file_name(page_url: str) -> str:
     split = urlsplit(page_url)
     nl = re.sub(r'[\W]', '-', split.netloc)
     pa = re.sub(r'[\W]', '-', split.path)
     jj = [nl, pa, '.html']
+    return ''.join(jj)
+
+
+def create_dir_name(page_url: str) -> str:
+    split = urlsplit(page_url)
+    nl = re.sub(r'[\W]', '-', split.netloc)
+    pa = re.sub(r'[\W]', '-', split.path)
+    jj = [nl, pa, '_files']
     return ''.join(jj)
