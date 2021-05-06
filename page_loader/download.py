ff77f3e98a51a85eb432eba9d96b9eb9265c69d3
AABur <aabur@mail.ru>
2021-05-06 08:14:47 +0200

    fix: :bug: uodate create file name

--- page_loader/download.py
+++ page_loader/download.py
@@ -1,32 +1,35 @@
+import os
 import re
 from pathlib import Path, PurePath
-from urllib.parse import urlsplit
+from urllib.parse import urlparse, urlsplit
 
 import requests
 
 
 def download(page_url: str, output_dir: str) -> Path:
-    page_url.rstrip('.')
-    print(page_url)
-    file_name = create_file_name(page_url)
+    file_name = generate_file_name(page_url)
     file_path = Path(PurePath(output_dir, file_name))
     response = requests.get(page_url)
     file_path.write_bytes(response.content)
-    dir_name = create_dir_name(page_url)
+    dir_name = generate_dir_name(page_url)
     Path(output_dir, dir_name).mkdir(exist_ok=True)
     return file_path
 
 
-def create_file_name(page_url: str) -> str:
-    split = urlsplit(page_url)
-    nl = re.sub(r'[\W]', '-', split.netloc)
-    pa = re.sub(r'[\W]', '-', split.path)
-    jj = [nl, pa, '.html']
-    return ''.join(jj)
+def generate_file_name(url):
+    result_url_parse = urlparse(url)
+    path, ext = os.path.splitext(result_url_parse.path)
+    filename = replace_chars(result_url_parse.netloc + path)
+    ext = ext or '.html'
+    return filename + ext
 
 
-def create_dir_name(page_url: str) -> str:
-    split = urlsplit(page_url)
+def replace_chars(s):
+    return re.sub(re.compile(r'[^0-9a-zA-Z]+'), '-', s)
+
+
+def generate_dir_name(url_name: str) -> str:
+    split = urlsplit(url_name)
     nl = re.sub(r'[\W]', '-', split.netloc)
     pa = re.sub(r'[\W]', '-', split.path)
     jj = [nl, pa, '_files']
