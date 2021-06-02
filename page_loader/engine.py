4fa0fca8b06cf3c5a77aa98c6107c8520f023cf7
AABur <aabur@mail.ru>
2021-06-02 22:53:19 +0300

    refactor: :recycle: refactor code

--- page_loader/engine.py
+++ page_loader/engine.py
@@ -32,97 +32,70 @@ def download(page_url: str, target_dir: str = '') -> str:
     """
     local_page_name = compose_local_name(page_url)
     assets_dir_name = compose_local_name(page_url, 'dir')
-    page_file_path = Path(Path.cwd(), target_dir, local_page_name)
-    assets_dir_path = Path(Path.cwd(), target_dir, assets_dir_name)
-    logger.debug(
-        'Start downloading {0} to {1}'.format(page_url, page_file_path))
-    logger.debug('111')
+    page_file_path = Path(target_dir, local_page_name)
+    assets_local_dir = Path(target_dir, assets_dir_name)
+    logger.debug(f'Start downloading {page_url} to {page_file_path}')
     try:
-        Path(assets_dir_path).mkdir(exist_ok=True)
+        Path(assets_local_dir).mkdir(exist_ok=True)
     except Exception:
         logger.error('Failed to mkdir', exc_info=True)
         sys.exit(1)
     # collect remote assets and prepare local html-page
-    logger.debug('222')
-    try:
-        local_html = fetch_assets(
-            page_url,
-            assets_dir_name,
-            assets_dir_path,
-        )
-    except Exception:
-        logger.error('Failed to fetch_assets', exc_info=True)
-        sys.exit(1)
-    logger.debug('333')
+    local_html = fetch_assets(page_url, assets_dir_name, assets_local_dir)
     try:
         Path(page_file_path).write_text(local_html)  # save html-page locally
     except Exception:
         logger.error('Failed to write file', exc_info=True)
         sys.exit(1)
-    logger.debug('Finish downloading {0} to {1}'.format(
-        page_url, page_file_path,
-    ))
+    logger.debug('Finish downloading {page_url} to {page_file_path}')
     return str(page_file_path)
 
 
-def fetch_assets(
-    page_url: str,
-    assets_dir_name: str,
-    assets_path: Path,
-) -> Any:  # bs4 don't have type stub
+def fetch_assets(page_url: str, assets_dir_name: str, assets_local_dir: Path) -> Any:  # noqa:WPS210,E501
     """Download assets from given HTML page and store it in assets directory.
 
     Args:
         page_url (str): given HTML page url
         assets_dir_name (str): assets directory
-        assets_path (Path): [description]
+        assets_local_dir (Path): [description]
 
     Returns:
         Any: [description]
     """
     logger.debug('Start downloading assets')
     soup = BeautifulSoup(requests.get(page_url).text, 'lxml')
-    tags_list = soup.find_all(TAGS)
-    bar = IncrementalBar(
+    page_url = page_url if page_url.endswith('/') else f'{page_url}/'
+    with IncrementalBar(
         'Downloading',
-        max=len(tags_list),
+        max=len(soup.find_all(TAGS)),
         suffix='%(percent).1f%% [%(elapsed)ds]',  # noqa:WPS323
-    )
-    if not page_url.endswith('/'):
-        page_url = '{0}/'.format(page_url)
-
-    for source_tag in tags_list:
-        attribute_name = 'src' if source_tag.name in {
-            'script', 'img',
-        } else 'href'
-        asset_url = source_tag.get(attribute_name)
-        if not asset_url:
-            bar.next()
-            continue
-        full_asset_url = urljoin(page_url, asset_url)
-        logger.debug(full_asset_url)
-        if urlparse(full_asset_url).netloc == urlparse(page_url).netloc:
-            local_file_name = compose_local_name(full_asset_url)
-            full_asset_path = Path(assets_path, local_file_name)
-            logger.debug('Start downloading assets {0}'.format(full_asset_url))
-            try:
-                asset_content = requests.get(
-                    full_asset_url, stream=True,
-                ).content
-            except Exception:
-                logger.error('Failed to access to asset', exc_info=True)
-                sys.exit(1)
-            logger.debug('555')
-            try:
-                Path(full_asset_path).write_bytes(asset_content)
-            except Exception:
-                logger.error('Failed to write asset file', exc_info=True)
-                sys.exit(1)
-            source_tag[attribute_name] = Path(assets_dir_name, local_file_name)
-        bar.next()
-    bar.finish()
+    ) as bar:
+        for source_tag in soup.find_all(TAGS):
+            attribute_name = 'src' if source_tag.name in {
+                'script', 'img',
+            } else 'href'
+            full_asset_url = urljoin(page_url, source_tag.get(attribute_name))
+            if urlparse(full_asset_url).netloc == urlparse(page_url).netloc:
+                local_file_name = compose_local_name(full_asset_url)
+                try:
+                    get_asset(assets_local_dir, full_asset_url, local_file_name)
+                except ConnectionError:
+                    logger.error('Failed access asset', exc_info=True)
+                    sys.exit(1)
+                except IOError:
+                    logger.error('Failed write asset file', exc_info=True)
+                    sys.exit(1)
+                source_tag[attribute_name] = Path(
+                    assets_dir_name, local_file_name,
+                )
+            bar.next()  # noqa:B305
     logger.debug('Finish downloading assets')
     return soup.prettify(formatter='html5')
+
+
+def get_asset(assets_local_dir, full_asset_url, local_file_name):
+    asset_content = requests.get(full_asset_url).content
+    Path(assets_local_dir, local_file_name).write_bytes(asset_content)
 
 
 def compose_local_name(resource_url: str, resource_type: str = '') -> str:
