f360c0d1f2a8342dd52bf4698f155e9a48453812
AABur <aabur@mail.ru>
2021-07-30 23:25:55 +0300

    refactor: :recycle: config logger with dictConfig()

--- page_loader/scripts/main.py
+++ page_loader/scripts/main.py
@@ -2,29 +2,51 @@
 # -*- coding:utf-8 -*-
 """Download a page from the command line."""
 
-
 import logging
 
 from page_loader import download
 from page_loader.cli import arg_parser
 
-logger = logging.getLogger('page-loader')
-logger.setLevel(logging.DEBUG)
-# create file handler which logs even debug messages
-fh = logging.FileHandler('page-loader.log')
-fh.setLevel(logging.DEBUG)
-# create console handler with a higher log level
-ch = logging.StreamHandler()
-ch.setLevel(logging.INFO)
-# create formatter and add it to the handlers
-formatter = logging.Formatter(
-    '%(asctime)s - %(name)s - %(levelname)s - %(message)s',   # noqa:WPS323
-)
-fh.setFormatter(formatter)
-ch.setFormatter(formatter)
-# add the handlers to the logger
-logger.addHandler(fh)
-logger.addHandler(ch)
+logger_config = {
+    'version': 1,
+    'disable_existing_loggers': True,
+    'formatters':
+        {
+            'default':
+            {
+                'format': '%(asctime)s - %(name)s - %(levelname)s - %(message)s'  # noqa: C812, E501, WPS323
+            },
+        },
+    'handlers':
+        {
+            'console':
+            {
+                'class': 'logging.StreamHandler',
+                'formatter': 'default',
+                'level': 'INFO',
+            },
+            'file':
+            {
+                'class': 'logging.FileHandler',
+                'formatter': 'default',
+                'filename': 'page-loader.log',
+                'level': 'DEBUG',
+            },
+        },
+    'loggers':
+        {
+            'page-loader':
+            {
+                'handlers': ['console', 'file'],
+                'level': 'DEBUG',
+                'propagate': True,
+            },
+        },
+}
+
+logging.coding.dictConfig(logger_config)
+
+logger = logging.getLogger(__name__)
 
 
 def main():
